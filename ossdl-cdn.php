e48b5eceeedbdaaaa077a14bbe8087b7d4b0fbfa
W-Mark Kubacki <wmark@hurrikane.de>
2010-07-09 18:42:39 +0000

    whitespace changes

--- ossdl-cdn.php
+++ ossdl-cdn.php
@@ -59,30 +59,28 @@ function ossdl_off_menu() {
 }
 
 function ossdl_off_options() {
-if ( isset($_POST['action']) && ( $_POST['action'] == 'update_ossdl_off' )){
-	update_option('ossdl_off_cdn_url', $_POST['ossdl_off_cdn_url']);
-}
+	if ( isset($_POST['action']) && ( $_POST['action'] == 'update_ossdl_off' )){
+		update_option('ossdl_off_cdn_url', $_POST['ossdl_off_cdn_url']);
+	}
 
-?>
-<div class="wrap">
-<h2>OSSDL CDN off-linker</h2>
-<p>Many Wordpress plugins misbehave when linking to their JS or CSS files, and yet there is no filter to let your old posts point to a statics' site or CDN for images.
-Therefore this plugin replaces at any links into <code>wp-content</code> and <code>wp-includes</code> directories (except for PHP files) the <code>blog_url</code> by the URL you provide below.
-That way you can either copy all the static content to a dedicated host or mirror the files at a CDN by <a href="http://knowledgelayer.softlayer.com/questions/365/How+does+Origin+Pull+work%3F" target="_blank">origin pull</a>.</p>
-<p><strong style="color: red">WARNING:</strong> Test some static urls e.g., <code>http://static.mydomain.com/wp-includes/js/prototype.js</code> to ensure your CDN service is fully working before saving changes.</p>
-<p><form method="post" action="">
-<table class="form-table">
-<tr valign="top">
-<th scope="row"><label for="ossdl_off_cdn_url">off-site URL</label></th>
-<td><input type="text" name="ossdl_off_cdn_url" value="<?php echo get_option('ossdl_off_cdn_url'); ?>" size="64" /></td>
-<td><span class="setting-description">The new URL to be used in place of <?php echo get_option('siteurl'); ?> for rewriting. E.g. <code><?php echo str_replace('http://', 'http://cdn.', str_replace('www.', '', get_option('siteurl'))); ?></code>.</span></td>
-</tr>
-</table>
-<input type="hidden" name="action" value="update_ossdl_off" />
-<p class="submit">
-<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
-</p>
-</form></p>
-</div>
-<?php
+	?><div class="wrap">
+		<h2>OSSDL CDN off-linker</h2>
+		<p>Many Wordpress plugins misbehave when linking to their JS or CSS files, and yet there is no filter to let your old posts point to a statics' site or CDN for images.
+		Therefore this plugin replaces at any links into <code>wp-content</code> and <code>wp-includes</code> directories (except for PHP files) the <code>blog_url</code> by the URL you provide below.
+		That way you can either copy all the static content to a dedicated host or mirror the files at a CDN by <a href="http://knowledgelayer.softlayer.com/questions/365/How+does+Origin+Pull+work%3F" target="_blank">origin pull</a>.</p>
+		<p><strong style="color: red">WARNING:</strong> Test some static urls e.g., <code>http://static.mydomain.com/wp-includes/js/prototype.js</code> to ensure your CDN service is fully working before saving changes.</p>
+		<p><form method="post" action="">
+		<table class="form-table"><tbod>
+			<tr valign="top">
+				<th scope="row"><label for="ossdl_off_cdn_url">off-site URL</label></th>
+				<td>
+					<input type="text" name="ossdl_off_cdn_url" value="<?php echo get_option('ossdl_off_cdn_url'); ?>" size="64" class="regular-text code" />
+					<span class="description">The new URL to be used in place of <?php echo get_option('siteurl'); ?> for rewriting. E.g. <code><?php echo str_replace('http://', 'http://cdn.', str_replace('www.', '', get_option('siteurl'))); ?></code>.</span>
+				</td>
+			</tr>
+		</tbody></table>
+		<input type="hidden" name="action" value="update_ossdl_off" />
+		<p class="submit"><input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" /></p>
+		</form></p>
+	</div><?php
 }
