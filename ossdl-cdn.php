764d00922382880b7f84096510a91c23b0321bd5
W-Mark Kubacki <wmark@hurrikane.de>
2010-07-09 18:04:27 +0000

    formatting of administrative page

--- ossdl-cdn.php
+++ ossdl-cdn.php
@@ -69,8 +69,7 @@ if ( isset($_POST['action']) && ( $_POST['action'] == 'update_ossdl_off' )){
 <p>Many Wordpress plugins misbehave when linking to their JS or CSS files, and yet there is no filter to let your old posts point to a statics' site or CDN for images.
 Therefore this plugin replaces at any links into <code>wp-content</code> and <code>wp-includes</code> directories (except for PHP files) the <code>blog_url</code> by the URL you provide below.
 That way you can either copy all the static content to a dedicated host or mirror the files at a CDN by <a href="http://knowledgelayer.softlayer.com/questions/365/How+does+Origin+Pull+work%3F" target="_blank">origin pull</a>.</p>
-<p><strong style="color: red">WARNING:</strong> Test some static urls e.g., http://static.mydomain.com/wp-includes/js/prototype.js<br/>
-to ensure your CDN service is fully working before saving changes.</p>
+<p><strong style="color: red">WARNING:</strong> Test some static urls e.g., <code>http://static.mydomain.com/wp-includes/js/prototype.js</code> to ensure your CDN service is fully working before saving changes.</p>
 <p><form method="post" action="">
 <table class="form-table">
 <tr valign="top">
