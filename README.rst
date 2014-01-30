9636e14c523a5dae297983bee960f0b8b72e61ab
W-Mark Kubacki <wmark@hurrikane.de>
2014-01-30 17:47:15 +0100

    Improve grammar, wording, clarity.

--- README.rst
+++ README.rst
@@ -11,76 +11,83 @@ Rewrites links to static files to your own CDN network.
 
 Description
 ============
-Replaces the blog URL by another for all files under `wp-content` and `wp-includes`.
-That way static content can be handled by a CDN by origin pull. (The origin is your blog address.)
+Modifies links pointing to ‘wp-content’ and/or ‘wp-includes’ (or whatever you configure) 
+by replacing your ‘blog_url’ with a custom one.
+Enables you to pull static files, such as images, CSS or JS, from a different host, mirror or CDN.
 
-You could upload your static files to S3, CloudFront or just any site, too.
+You could upload your files to S3, CloudFront or a host dedicated to serving static files.
 For S3/Cloudfront, see this script_ to help you with uploading/synchronizing your blog files.
 
-The **lite version** of the plugin works like the **regular version** but is not supported.
-You can get the *regular version* at Github_ which:
-
-* Is free of charge, open source and without any ads.
-* Supported. Here is `the bug tracker`__ if you encounter any issues.
-* Has more and advanced features.
-* Is updated more frequently. See downloads_.
-* Quality assurance.
-
 .. _script:    http://mark.ossdl.de/2009/09/how-to-copy-your-wordpress-files-to-cloudfront-efficiently/
-.. _Github:    http://github.com/wmark/CDN-Linker
-.. _issues:    http://github.com/wmark/CDN-Linker/issues
-.. _downloads: http://github.com/wmark/CDN-Linker/downloads
-__ issues_
 
 License
 ========
 Licensed under the Reciprocal Public License, Version 1.5, for Personal Use as defined in 1.11 therein
 (http://www.opensource.org/licenses/rpl1.5).
 
-Else please contact me by email for an individual license.
+Else, please contact me by email for an individual license.
 
 Installation
 =============
 
-1. Setup your CDN: Either configure an origin pull, mirror bucket or upload your static files somewhere.
+1. Setup your CDN: Either configure “origin pull”, “mirror bucket” or upload your static files to a dedicated host.
 2. Upload the plugin to your `/wp-content/plugins/` directory.
-3. Activate the plugin through the 'Plugins' menu in WordPress.
-4. Provide the URL, where your static files can be found, under `Settings`.
+3. Activate the plugin through the ‘Plugins’ menu in WordPress.
+4. Provide the URL, where your static files can be found, under ‘Settings’.
 
 Frequently Asked Questions
 ===========================
 
+Is it free of charge to use?
+  Yes, for personal use. ‘Personal use’ excludes any commercial or commercialized sites or blogs.
+  Although ‘personal use’ excludes hosting companies you are free to upload the plugin to your WP installation individually, though,
+  as long as your “WP installation” is covered by the term ‘personal use’. ;-)
+
+  You can obtain a generous license for other use-cases for a tip.
+  By ‘generous’ I mean that one license will cover all domains serving the same content no matter which TLD
+  (myblog.com, myblog.de, myblog.us — all covered) and the such.
+  Just drop me an email.
+
+  Contributors will receive a license free of charge.
+
 How to uninstall?
-  Either deactivate the plugin or delete the plugin's directory.
+  Either deactivate the plugin or delete the plugin’s directory.
 
 Why another such plugin?
-  As many WP plugins don't correctly include JS and CSS files most of the current CDN plugins cannot
-  rewrite links correctly. They rely on the high-level WP API.
+  Most plugins for WP don’t correctly include JS and CSS files by using Wordpress’ functions.
+  This results in you not being able to pull static files, such as images, CSS or JS, from a different host, mirror or CDN.
 
-  This plugin does its rewriting on the lowest level possible - PHP itself.
+  This plugin does its rewriting on the lowest level possible — PHP itself —
+  providing a global work-around.
 
 How does it work?
-  After your blog pages have been rendered but before sending them to the visitor,
-  it will rewrite links pointing to `wp-content` and `wp-includes`. That rewriting will simply
-  replace your blog URL with that you have entered in settings screen.
-
-  Thus files are pulled from the other site.
+  After your blog pages have been rendered, but before WP is sending them to the visitor,
+  CDN Linker will modify links pointing to `wp-content` and `wp-includes` by
+  replacing your blog URL.
 
 Is it compatible to plugin XY?
-  Yes, by design it is compatible to all plugins. It hooks into a PHP function ob_start_
-  and there does the string replacement. Therefore, no Wordpress function is altered, overwritten or modified in any way.
+  Yes, by design it is compatible with all plugins. It hooks into PHP function ob_start_
+  and does the string replacement there. No Wordpress function is altered, overwritten or modified in any way.
 
-  Indeed, you could even copy this plugin's rewriter into any arbitrary PHP software.
+  Indeed, you could even copy this plugin’s rewriter into any arbitrary PHP software.
 
 Will it work with my CDN?
-  Yes, **if it supports origin pull** (aka mirror bucket, some sort of caching). Or if you upload your files manually.
+  Yes, **if it supports “origin pull”** (aka “mirror bucket”, some sort of caching). Or if you upload your files manually.
 
-What about Amazon's S3?
-  Right, S3 doesn't support origin pull. You will have to upload your files by hand.
-  I've written a script for this, too, so it is just a matter of running it. It is linked on the bottom of my blog post about
+What about Amazon’s S3?
+  Right, S3 doesn’t support “origin pull”. You will have to upload your files by hand.
+  I’ve written a script for this, too, so it is just a matter of running it. It is linked on the bottom of my blog post about
   `copying files to S3 <http://mark.ossdl.de/2009/09/how-to-copy-your-wordpress-files-to-cloudfront-efficiently/>`_.
 
-  Amazon's CloudFront does support origin pull. Just create a distribution as usual and set your blog's URL as origin.
+  Amazon’s CloudFront does support origin pull. Just create a “distribution” as usual and set your blog’s URL as “origin”.
+
+So my blog is fully served by a CDN?
+  No. Wordpress will still have to run on the host to which your address resolves to.
+  “www.myblog.blog” will still have to be “www.myblog.blog”, only your (for example) images will be loaded
+  from “cdn.myblog.blog”. Most CDNs do not support fast-changing content or passthrough of HTTP POST requests (→ comments).
+
+There is more than one “CDN Linker”?
+  Yes, this is the regular version. There is a “professional” and a “multisite” version, too.
 
 What other plugins do you recommend?
   Now that you can offload all the files such as images, music or CSS, you should serve your blog posts as static files to
--- wp-cdn-linker.php
+++ wp-cdn-linker.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: CDN Linker
 Plugin URI: https://github.com/wmark/CDN-Linker
-Description: Replaces the blog URL by another for all files under <code>wp-content</code> and <code>wp-includes</code>. That way static content can be handled by a CDN by origin pull - the origin being your blog address - or loaded from an other site.
+Description: Modifies links pointing to <code>wp-content</code> and/or <code>wp-includes</code> (or whatever you configure) by replacing your ‘blog_url’ with a custom one. Enables you to pull static files, such as images, CSS or JS, from a different host, mirror or CDN.
 Version: 1.4.1
 Author: W-Mark Kubacki
 Author URI: http://mark.ossdl.de/
@@ -18,7 +18,7 @@ if ( @include_once('cdn-linker-base.php') ) {
 function ossdl_off_activate() {
 	add_option('ossdl_off_cdn_url', get_option('siteurl'));
 	add_option('ossdl_off_include_dirs', 'wp-content,wp-includes');
-	add_option('ossdl_off_exclude', '.php, https://');
+	add_option('ossdl_off_exclude', '.php');
 	add_option('ossdl_off_rootrelative', '');
 	add_option('ossdl_off_www_is_optional', '');
 	add_option('ossdl_off_disable_cdnuris_if_https', '1');
@@ -55,6 +55,7 @@ function ossdl_off_options() {
 			$excludes[] = '.php';
 			update_option('ossdl_off_exclude', implode(',', $excludes));
 		}
+		// checkboxes which are not checked are sometimes not sent, hence the additional calls to isset($_POST(…))
 		$ossdl_off_rootrelative = isset($_POST['ossdl_off_rootrelative']) ? !!$_POST['ossdl_off_rootrelative'] : false;
 		$ossdl_off_www_is_optional = isset($_POST['ossdl_off_www_is_optional']) ? !!$_POST['ossdl_off_www_is_optional'] : false;
 		$ossdl_off_disable_cdnuris_if_https = isset($_POST['ossdl_off_disable_cdnuris_if_https']) ? !!$_POST['ossdl_off_disable_cdnuris_if_https'] : false;
@@ -73,22 +74,25 @@ function ossdl_off_options() {
 		$example_cdn_uri = $cdn_strategy->get_for($example_uri) . $example_file_rr;
 	}
 
-	?><div class="wrap">
+	?><div class="wrap" xml:lang="en">
 		<h2>CDN Linker</h2>
-		<p>Many Wordpress plugins misbehave when linking to their JS or CSS files, and yet there is no filter to let your old posts point to a statics' site or CDN for images.
-		Therefore this plugin replaces at any links into <code>wp-content</code> and <code>wp-includes</code> directories (except for PHP files) the <code>blog_url</code> by the URL you provide below.
-		That way you can either copy all the static content to a dedicated host or mirror the files at a CDN by <a href="http://knowledgelayer.softlayer.com/questions/365/How+does+Origin+Pull+work%3F" target="_blank">origin pull</a>.</p>
-		<p><strong style="color: red">WARNING:</strong> Test some static urls e.g., <code><a href="<?php echo($example_cdn_uri); ?>" target="_blank"><?php echo($example_cdn_uri); ?></a></code> to ensure your CDN service is fully working before saving changes.</p>
+		<p>Many Wordpress plugins misbehave when linking to their JS or CSS files by not calling the appropriate functions from Wordpress.
+		This results in you not being able to pull static files, such as images, CSS or JS, from a different host, mirror or CDN.<p>
+		<p>Therefore this plugin modifies links pointing to <code>wp-content</code> and/or <code>wp-includes</code> by replacing your <code>blog_url</code> with the URL you provide below.
+		You will be able to lessen the load on machines running your WP installation,
+		and utilize CDNs providing <q>origin pull</q> or hosts dedicated to serving static files.</p>
+		<p><strong style="color: red">WARNING:</strong> Test some static urls e.&thinsp;g., <code><a href="<?php echo($example_cdn_uri); ?>" target="_blank"><?php echo($example_cdn_uri); ?></a></code> to ensure your CDN service is fully working before saving changes.</p>
 		<p><form method="post" action="">
 		<table class="form-table"><tbod>
 			<tr valign="top">
 				<th scope="row"><label for="ossdl_off_cdn_url">CDN URL</label></th>
 				<td>
 					<input type="text" name="ossdl_off_cdn_url" value="<?php echo(get_option('ossdl_off_cdn_url')); ?>" size="64" class="regular-text code" />
-					<span class="description">The new URL to be used in place of <?php echo(get_option('siteurl')); ?> for rewriting. No trailing <code>/</code> please. E.g. <code><?php echo($example_cdn_uri); ?></code>.
+					<span class="description">The new URL to be used in place of <?php echo(get_option('siteurl')); ?> for rewriting. No trailing <code>/</code> please. E.&thinsp;g. <code><?php echo($example_cdn_uri); ?></code>.
 					&mdash;
-					You can use <code>%4%</code> (number between 1 and 9, surrounded by percent signs) to enable up to that many hostname variations.
-					1 or less doesn't make sense and more than 4 is beyond optimum. If you are going to use 3 or more, then make sure they have different IPs or
+					You can use <code>%4%</code> (a number between 1 and 9, surrounded by percent signs) to use that many hostname variations.
+					Should be between 2 and 4, with 4 being beyond an universal optimum.
+					If you are going to use 3 or more then make sure they have different IPs or
 					<a href="http://statichtml.com/2010/use-unique-ips-for-sharded-asset-hosts.html">some routers will block requests</a> to them.
 					</span>
 				</td>
@@ -97,37 +101,40 @@ function ossdl_off_options() {
 				<th scope="row"><label for="ossdl_off_rootrelative">rewrite root-relative refs</label></th>
 				<td>
 					<input type="checkbox" name="ossdl_off_rootrelative" <?php echo(!!get_option('ossdl_off_rootrelative') ? 'checked="1" ' : '') ?>value="true" class="regular-text code" />
-					<span class="description">Check this if you want to have links like <code><em>/</em>wp-content/xyz.png</code> rewritten - i.e. without your blog's domain as prefix.</span>
+					<span class="description">Check this if you want to have links like <code><em>/</em>wp-content/xyz.png</code> rewritten - i.&thinsp;e. without your blog’s domain as prefix.</span>
 				</td>
 			</tr>
 			<tr valign="top">
-				<th scope="row"><label for="ossdl_off_www_is_optional">subdomain 'www' is optional</label></th>
+				<th scope="row"><label for="ossdl_off_www_is_optional">subdomain <q>www</q> is optional</label></th>
 				<td>
 					<input type="checkbox" name="ossdl_off_www_is_optional" <?php echo(!!get_option('ossdl_off_www_is_optional') ? 'checked="1" ' : '') ?>value="true" class="regular-text code" />
-					<span class="description">Check this if your blog can be accessed without a 'www' in front of its domain name. If unchecked, links with missing 'www' won't be modified.
-					Safe to say 'yes' here.</span>
+					<span class="description">Check this if your blog can be accessed without a <q>www</q> in front of its domain name. If unchecked links without a <q>www</q> won’t be modified.
+					Safe to say <q>yes</q> here.</span>
 				</td>
 			</tr>
 			<tr valign="top">
-				<th scope="row"><label for="ossdl_off_disable_cdnuris_if_https">HTTPS without CDN</label></th>
+				<th scope="row"><label for="ossdl_off_disable_cdnuris_if_https">skip CDN if HTTPS</label></th>
 				<td>
 					<input type="checkbox" name="ossdl_off_disable_cdnuris_if_https" <?php echo(!!get_option('ossdl_off_disable_cdnuris_if_https') ? 'checked="1" ' : '') ?>value="true" class="regular-text code" />
 					<span class="description">Skips linking to your CDN if the page has been visited using HTTPS. This option will not affect caching.
-					If in doubt say 'yes'.</span>
+					If in doubt say <q>yes</q>. Say <q>no</q> if your CDN supports HTTPS.</span>
 				</td>
 			</tr>
 			<tr valign="top">
 				<th scope="row"><label for="ossdl_off_include_dirs">include dirs</label></th>
 				<td>
 					<input type="text" name="ossdl_off_include_dirs" value="<?php echo(get_option('ossdl_off_include_dirs')); ?>" size="64" class="regular-text code" />
-					<span class="description">Directories to include in static file matching. Use a comma as the delimiter. Default is <code>wp-content, wp-includes</code>, which will be enforced if this field is left empty.</span>
+					<span class="description">Directories to include in static file matching.
+					Use a comma as delimiter. Default is <code>wp-content, wp-includes</code>, which will be enforced if this field is left empty.</span>
 				</td>
 			</tr>
 			<tr valign="top">
 				<th scope="row"><label for="ossdl_off_exclude">exclude if substring</label></th>
 				<td>
 					<input type="text" name="ossdl_off_exclude" value="<?php echo(get_option('ossdl_off_exclude')); ?>" size="64" class="regular-text code" />
-					<span class="description">Excludes something from being rewritten if one of the above strings is found in the match. Use a comma as the delimiter. E.g. <code>.php, .flv, .do</code>, always include <code>.php, https://</code>, which is the default. (Will be <code>.php</code> if left empty.)</span>
+					<span class="description">Excludes something from being rewritten if one of the above strings is found in the match.
+					Use a comma as delimiter. E.&thinsp;g. <code>.php, .flv, .do</code>.
+					Always include <code>.php</code>, which is the default. (Will be set to <code>.php</code> if left empty.)</span>
 				</td>
 			</tr>
 		</tbody></table>
