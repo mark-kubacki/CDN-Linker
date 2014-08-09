5df603acc11425b5b1eccbed194608a726f9f414
W-Mark Kubacki <wmark@hurrikane.de>
2014-08-10 00:37:06 +0200

    Switch from PHPUnit to Atoum.

--- test.php
+++ test.php
@@ -2,15 +2,23 @@
 /*
  * These tests need PHP's "zlib" extension.
  *
- * Run with: phpunit test.php
+ * Run with:
+ *     php -d phar.readonly=0 mageekguy.atoum.phar --update || \
+ *     curl -LOR http://downloads.atoum.org/nightly/mageekguy.atoum.phar
+ *     php test.php
  */
+
+namespace blitznote\wp\cdn\tests\units;
+
+require_once('mageekguy.atoum.phar');
 
 include('cdn-linker-base.php');
 
-class CDNLinkerTest extends PHPUnit_Framework_TestCase
+use \mageekguy\atoum;
+use \blitznote\wp\cdn;
+
+class URI_changer extends atoum\test
 {
-	/** Instance of URI_changer. */
-	var $ctx = null;
 
 	public function readCompressedSample($filename) {
 		$fp = gzopen('test/samples/'.$filename, 'r') or die("can't open: $php_errormsg");
@@ -27,70 +35,78 @@ class CDNLinkerTest extends PHPUnit_Framework_TestCase
 		gzclose($fp);
 	}
 
-	protected function setUp() {
-		$this->ctx = new blitznote\wp\cdn\URI_changer(
+	protected function getInstance() {
+		$uri_changer = new cdn\URI_changer(
 			'http://test.local',
-			blitznote\wp\cdn\target_url_strategy_for('http://cdn.test.local'),
+			cdn\target_url_strategy_for('http://cdn.test.local'),
 			'wp-content,wp-includes',
 			array('.php'),
 			false,
 			false,
 			true
 		);
-		$this->ctx->in_unit_test = true;
+		$uri_changer->in_unit_test = true;
+		return $uri_changer;
 	}
 
 	public function testNoModificationIfUrlsMatch() {
-		$this->ctx->get_target_url = blitznote\wp\cdn\target_url_strategy_for('http://test.local');
+		$uri_changer = $this->getInstance();
+		$uri_changer->get_target_url = cdn\target_url_strategy_for('http://test.local');
 		$input = '<a href="http://test.local/favicon.ico">some text</a>';
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($input, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($input);
 	}
 
 	public function testModifiesIfUrlsDontMatch() {
+		$uri_changer = $this->getInstance();
 		$input = '<a href="http://test.local/favicon.ico">some text</a>';
 		$expected = '<a href="http://cdn.test.local/favicon.ico">some text</a>';
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 	}
 
 	public function testRootRelativeLinkDisabled() {
-		$this->ctx->rootrelative = false;
+		$uri_changer = $this->getInstance();
+		$uri_changer->rootrelative = false;
 		$input = '<a href="/favicon.ico">some text</a>';
 		$expected = '<a href="/favicon.ico">some text</a>';
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 	}
 
 	public function testRootRelativeLinkEnabled() {
-		$this->ctx->rootrelative = true;
+		$uri_changer = $this->getInstance();
+		$uri_changer->rootrelative = true;
 		$input = '<a href="/favicon.ico"><img src="http://test.local/favicon.ico" /></a>';
 		$expected = '<a href="http://cdn.test.local/favicon.ico"><img src="http://cdn.test.local/favicon.ico" /></a>';
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 	}
 
 	public function testLinksToPostsAndPagesNotAffected() {
+		$uri_changer = $this->getInstance();
 		$expected = $input = $this->readCompressedSample('virtual-1.before.gz');
 
-		$this->ctx->rootrelative = false;
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$uri_changer->rootrelative = false;
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 
-		$this->ctx->rootrelative = true;
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$uri_changer->rootrelative = true;
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 	}
 
 	public function testExcludes() {
-		$this->ctx->rootrelative = true;
+		$uri_changer = $this->getInstance();
+		$uri_changer->rootrelative = true;
 		$input = '<a href="/favicongetter.php"><img src="/favicon.ico" /></a>';
 		$expected = '<a href="/favicongetter.php"><img src="http://cdn.test.local/favicon.ico" /></a>';
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 	}
 
 	public function testExcludesSomeMore() {
+		$uri_changer = $this->getInstance();
 		// This test is an attempt to reproduce issue #4.
 		// It is very verbosely commented because I want link to it whenever someone reports
 		// a similar issue. For that it needs to be understood even by non-programmers - or me
@@ -117,14 +133,14 @@ class CDNLinkerTest extends PHPUnit_Framework_TestCase
 		);
 		foreach($exclude_configurations as $excludes) {
 			foreach(array(true, false) as $rootrelative) {
-				$this->ctx->excludes = $excludes;
-				$this->ctx->rootrelative = $rootrelative;
+				$uri_changer->excludes = $excludes;
+				$uri_changer->rootrelative = $rootrelative;
 
 				foreach($cases as $input) {
 					$expected = $input;
 					// Output has to be unmodified.
-					$output = $this->ctx->rewrite($input);
-					$this->assertEquals($expected, $output);
+					$output = $uri_changer->rewrite($input);
+					$this->string($output)->isEqualTo($expected);
 					// If it were modified, e.g. comments wouldn't work or similar things.
 				}
 			}
@@ -132,45 +148,48 @@ class CDNLinkerTest extends PHPUnit_Framework_TestCase
 	}
 
 	public function testDisableCDNURISifHTTPS() {
-		$this->ctx->https_deactivates_rewriting = true;
+		$uri_changer = $this->getInstance();
+		$uri_changer->https_deactivates_rewriting = true;
 
 		// request without HTTPS
 		$input = '<img src="http://test.local/favicon.ico" />';
 		$expected = '<img src="http://cdn.test.local/favicon.ico" />';
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 
 		// request using HTTPS
 		$_SERVER["HTTPS"] = 'on';
 		$expected = '<img src="http://test.local/favicon.ico" />';
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 	}
 
 	public function testMultipleCDNs() {
-		$this->ctx->rootrelative = true;
-		$this->ctx->get_target_url = blitznote\wp\cdn\target_url_strategy_for('http://cdn%4%.test.local');
+		$uri_changer = $this->getInstance();
+		$uri_changer->rootrelative = true;
+		$uri_changer->get_target_url = cdn\target_url_strategy_for('http://cdn%4%.test.local');
 		$input = '<img src="/me.jpg" /><img src="/favicon.tif" /><a href="/movie.ogg">text</a><a href="/wp-content/file.exe">text</a>';
 		$expected = '<img src="http://cdn1.test.local/me.jpg" /><img src="http://cdn2.test.local/favicon.tif" /><a href="http://cdn3.test.local/movie.ogg">text</a><a href="http://cdn4.test.local/wp-content/file.exe">text</a>';
 
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 		// another run to make sure it is deterministic
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 	}
 
 	/** Now come actual sites. */
 
 	protected function comparePair($blog_url, $cdn_url, $root_relative, $fn_before, $fn_after) {
-		$this->ctx->blog_url = $blog_url;
-		$this->ctx->get_target_url = blitznote\wp\cdn\target_url_strategy_for($cdn_url);
-		$this->ctx->rootrelative = $root_relative;
+		$uri_changer = $this->getInstance();
+		$uri_changer->blog_url = $blog_url;
+		$uri_changer->get_target_url = cdn\target_url_strategy_for($cdn_url);
+		$uri_changer->rootrelative = $root_relative;
 
 		$input = $this->readCompressedSample($fn_before);
 		$expected = $this->readCompressedSample($fn_after);
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 	}
 
 	public function testImagesAtRootLevel() {
@@ -186,82 +205,86 @@ class CDNLinkerTest extends PHPUnit_Framework_TestCase
 	}
 
 	public function testExcludes2() {
+		$uri_changer = $this->getInstance();
 		// Howdyargs.com has (in this version) exceptionally ugly code.
 		// One plugin, fp-autoconnect (?), breaks w/o the particular excludes.
-		$this->ctx->blog_url = 'http://howdyags.com';
-		$this->ctx->get_target_url = blitznote\wp\cdn\target_url_strategy_for('http://cdn.howdyags.com');
-		$this->ctx->excludes = array('.php', 'xd_receiver.htm');
-		$this->ctx->rootrelative = true;
+		$uri_changer->blog_url = 'http://howdyags.com';
+		$uri_changer->get_target_url = cdn\target_url_strategy_for('http://cdn.howdyags.com');
+		$uri_changer->excludes = array('.php', 'xd_receiver.htm');
+		$uri_changer->rootrelative = true;
 
 		$input = $this->readCompressedSample('howdyags.com-before.gz');
 		$expected = $this->readCompressedSample('howdyags.com-after.gz');
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 
 		$input = $expected = '<a href="http://howdyags.com/groups/site-news/">Site News</a></div> ';
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 	}
 
 	public function testExcludes3() {
+		$uri_changer = $this->getInstance();
 		// 刘晖 has reported issue #4 and sent pages to reproce the issue.
 		// Let's see what has been configured to reproduce the incorrect output:
-		$this->ctx->blog_url = 'http://wangma.me';
-		$this->ctx->get_target_url = blitznote\wp\cdn\target_url_strategy_for('http://cdn.wangma.me');
+		$uri_changer->blog_url = 'http://wangma.me';
+		$uri_changer->get_target_url = cdn\target_url_strategy_for('http://cdn.wangma.me');
 
-		$this->ctx->excludes = array('.php'); //<-- this worked fine!
+		$uri_changer->excludes = array('.php'); //<-- this worked fine!
 		$input = $this->readCompressedSample('wangma.me-before.gz');
 		$expected = $this->readCompressedSample('wangma.me-after.gz');
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 
-		$this->ctx->excludes = array(); //<-- this is what actually has been configured and caused the malfunction
+		$uri_changer->excludes = array(); //<-- this is what actually has been configured and caused the malfunction
 		$expected = $this->readCompressedSample('wangma.me-incorrect.gz');
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 	}
 
 	public function testIncludeDirs() {
+		$uri_changer = $this->getInstance();
 		// this one has root-relative links to "/extscripts/..."
-		$this->ctx->blog_url = 'http://screenrant.com';
-		$this->ctx->get_target_url = blitznote\wp\cdn\target_url_strategy_for('http://cdn.screenrant.com');
-		$this->ctx->include_dirs = 'wp-content,wp-includes,extscripts';
-		$this->ctx->rootrelative = true;
+		$uri_changer->blog_url = 'http://screenrant.com';
+		$uri_changer->get_target_url = cdn\target_url_strategy_for('http://cdn.screenrant.com');
+		$uri_changer->include_dirs = 'wp-content,wp-includes,extscripts';
+		$uri_changer->rootrelative = true;
 
 		$input = $this->readCompressedSample('screenrant.com-before.gz');
 		$expected = str_replace('/extscripts/', 'http://cdn.screenrant.com/extscripts/',
 					$this->readCompressedSample('screenrant.com-after.gz'));
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 	}
 
 	public function testMissingWww() {
+		$uri_changer = $this->getInstance();
 		// http://prettyshinysparkly.com/
 		// 2012-01-30: everything under "/images/" doesn't work if root-relative == false
-		$this->ctx->blog_url = 'http://www.prettyshinysparkly.com';
-		$this->ctx->get_target_url = blitznote\wp\cdn\target_url_strategy_for('http://cdn.prettyshinysparkly.com');
-		$this->ctx->include_dirs = 'images, wp-content, wp-includes, js'; // spaces
-		$this->ctx->excludes = array_map('trim', explode(',', trim('.php, .flv, .do')));
+		$uri_changer->blog_url = 'http://www.prettyshinysparkly.com';
+		$uri_changer->get_target_url = cdn\target_url_strategy_for('http://cdn.prettyshinysparkly.com');
+		$uri_changer->include_dirs = 'images, wp-content, wp-includes, js'; // spaces
+		$uri_changer->excludes = array_map('trim', explode(',', trim('.php, .flv, .do')));
 
 		// tests for: rootrelative
 		$input = $this->readCompressedSample('prettyshinysparkly.com-before.gz');
 		$expected = $this->readCompressedSample('prettyshinysparkly.com-after.gz');
-		$this->ctx->rootrelative = false;
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
-		$this->ctx->rootrelative = true;
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$uri_changer->rootrelative = false;
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
+		$uri_changer->rootrelative = true;
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 
 		// tests for: www_is_optional
 		$input = $this->readCompressedSample('prettyshinysparkly.com-no_www-before.gz');
-		$this->ctx->www_is_optional = true;
-		$this->ctx->rootrelative = false;
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
-		$this->ctx->rootrelative = true;
-		$output = $this->ctx->rewrite($input);
-		$this->assertEquals($expected, $output);
+		$uri_changer->www_is_optional = true;
+		$uri_changer->rootrelative = false;
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
+		$uri_changer->rootrelative = true;
+		$output = $uri_changer->rewrite($input);
+		$this->string($output)->isEqualTo($expected);
 
 	}
 
