1619ffe0a438206cc884439fc0d340bebe0dd29a
W-Mark Kubacki <wmark@hurrikane.de>
2016-03-01 11:28:19 +0100

    Atoum: Upload test coverage only with PHP >= 7.0.0

--- .atoum.php
+++ .atoum.php
@@ -6,7 +6,8 @@ date_default_timezone_set('UTC');
 use mageekguy\atoum;
 use mageekguy\atoum\reports;
 
-if(getenv("COVERALLS_REPO_TOKEN")) {
+if(getenv('COVERALLS_REPO_TOKEN') &&
+   version_compare(PHP_VERSION, '7.0.0', '>=')) {
 	$coveralls = new reports\asynchronous\coveralls(".", getenv("COVERALLS_REPO_TOKEN"));
 	$coveralls->addDefaultWriter();
 
